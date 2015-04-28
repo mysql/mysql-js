c8c30ddf8c9cae78daf7bc0f0a146b496a7c3cd0
John David Duncan <john.duncan@oracle.com>
2015-04-28 16:14:25 -0700

    Adapt dbloader to use option handling from jones-test

--- loader/dbloader.js
+++ loader/dbloader.js
@@ -23,9 +23,10 @@
 global.debug = 0 ;  // FIXME, currently used by parser & scanner
 
 var assert = require("assert"),
-    mynode = require('database-jones'),
+    jones = require("database-jones"),
     LoaderJob = require("./lib/LoaderJob.js").LoaderJob,
-    LoaderModule = require("./lib/LoaderModule.js").LoaderModule
+    LoaderModule = require("./lib/LoaderModule.js").LoaderModule,
+    CommandLine = require("jones-test").CommandLine
 ;
 
 function usage(flagHandler) {
@@ -36,122 +37,58 @@ function usage(flagHandler) {
   process.exit(1);
 }
 
-
-/* Define a command-line option.
-   If takesValue is false, then container[valueName] will hold valueIfSet if
-   option is set; if takesValue is true, container[valueName] will hold
-   the next command-line parameter after option.
-*/
-function Option(shortForm, longForm, helpText, callback) {
-  this.shortForm  = shortForm;
-  this.longForm   = longForm;
-  this.helpText   = helpText;
-  this.callback   = callback;
-}
-
-Option.prototype.isOption = true;
-
-function FlagHandler() {
-  this.flags = {};
-  this.helpText = "";
-}
-
-FlagHandler.prototype.addOption = function(option) {
-  assert(option.isOption);
-  var shortFlag, longFlag, helpText;
-  helpText = "";
-  if(option.shortForm) {
-    shortFlag = option.shortForm;
-    this.flags[shortFlag] = option;
-  }
-  if(option.longForm) {
-    longFlag = option.longForm;
-    this.flags[longFlag] = option;
-  }
-  if(shortFlag && longFlag) {
-    helpText = "  " + longFlag + "\n" +
-               "  " + shortFlag + "     " + option.helpText + "\n";
-  } else if(shortFlag) {
-    helpText = "  " + shortFlag + "     " + option.helpText + "\n";
-  } else if (longFlag) {
-    helpText = "  " + longFlag + "  " +     option.helpText + "\n";
-  }
-
-  this.helpText += helpText;
-};
-
-FlagHandler.prototype.processArguments = function() {
-  var i, len, opt, nextArg, flag, consumed;
-  i = 2;
-  len = process.argv.length;
-  while(i < len) {
-    opt = process.argv[i];
-    nextArg = (i + 1 < len) ? process.argv[i+1] : null;
-    flag = this.flags[opt];
-    if(flag) {
-      consumed = flag.callback(nextArg);
-      if(consumed > 0) {
-        i += consumed;
-      } else {
-        usage(this);
-      }
-    } else {
-      usage(this);
-    }
-  }
-};
-
-
 function parse_command_line() {
   var i, len, handler, options;
-  handler = new FlagHandler();
+  handler = new CommandLine.FlagHandler();
+
   options = {
     "adapter" : "ndb",
     "plugin"  : new LoaderModule(handler)
   };
-  handler.addOption(new Option(
-    "-n", "--ndb", "Use NDB adapter", function(nextArg) {
-      options.adapter = "ndb";
-      return 1;
+
+  handler.addOption(new CommandLine.Option(
+    "-a", "--adapter=<adapter>",
+    "Use Jones database driver <adapter> (default: ndb)",
+    function(nextArg) {
+      if(typeof nextArg === 'string') {
+        options.adapter = nextArg;
+        return 1;
+      }
+      return -1;  // adapter is required
     }));
-  handler.addOption(new Option(
-   "-m", "--mysql", "Use MySQL adapter", function(nextArg) {
-      options.adapter = "mysql";
-      return 1;
-    }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     null, "--stats", "collect statistics", function(nextArg) {
       options.stats = true;
-      return 1;
+      return 0;
     }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     "-d", "--debug", "enable debugging output", function(nextArg) {
       unified_debug.on();
       unified_debug.level_debug();
-      return 1;
+      return 0;
       }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     null, "-df", "enable debugging output from <source_file>", function(nextArg) {
       unified_debug.on();
       unified_debug.set_file_level(nextArg, 5);
-      return 2;
+      return 1;
     }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     "-c", null, "<ndb_connect_string>", function(nextArg) {
       options.connect_string = nextArg;
-      return 2;
+      return 1;
     }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     "-f", null, "<control_file>", function(nextArg) {
       options.control_file = nextArg;
-      return 2;
+      return 1;
     }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     "-e", null, "<load_data_command_text>", function(nextArg) {
       options.control_text = nextArg;
-      return 2;
+      return 1;
     }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     "-j", null, "<javascript_plugin_file>", function(nextArg) {
       /* Here is the code that actually loads the user's module: */
       var modulePath;
@@ -162,9 +99,9 @@ function parse_command_line() {
         modulePath = "./" + modulePath;
       }
       require(modulePath).init(options.plugin);
-      return 2;
+      return 1;
     }));
-  handler.addOption(new Option(
+  handler.addOption(new CommandLine.Option(
     "-h", "--help", "show usage", function(nextArg) {
       usage(handler);
       process.exit(1);
@@ -206,7 +143,7 @@ function main() {
   }
 
   // Set connection properties
-  var connectionProperties = new mynode.ConnectionProperties(cmdOptions.adapter);
+  var connectionProperties = new jones.ConnectionProperties(cmdOptions.adapter);
 
   if(cmdOptions.connect_string) {
     connectionProperties.ndb_connectstring = cmdOptions.connect_string;
@@ -214,7 +151,7 @@ function main() {
   connectionProperties.database = job.destination.database;
 
   // Connect to the database and start the controller
-  mynode.openSession(connectionProperties, mappedConstructors, function(err, session) {
+  jones.openSession(connectionProperties, mappedConstructors, function(err, session) {
     if(err) throw err; /*JSON.stringify(err); process.exit(1);*/
     job.run(session, function onComplete(error, stats) {
       session.close().then(function() {
