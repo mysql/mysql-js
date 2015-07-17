080f30badfff1ce81500308b5d840065d6edd98b
John David Duncan <john.duncan@oracle.com>
2015-07-17 09:29:42 -0700

    Add option handling to run_std_bench.sh Use "git log" instead of "git show"

--- perftest/run_std_bench.sh
+++ perftest/run_std_bench.sh
@@ -1,15 +1,44 @@
-LOGDIR='./benchmark_logs'
-SUMMARYFILE="$LOGDIR/All-runs-summary.txt"
+#!/bin/bash
 
-REVNO=`git show --pretty=%h`   ## = "bzr revno"
+LOGDIR='./benchmark_logs'
+SUMMARYFILENAME="All-runs-summary.txt"
+NODE='node';
+DEPLOYMENT='test';
+
+usage () {
+  echo "Optional:";
+  echo "-E  deployment";
+  echo "-l  log dir";
+  echo "-n  node.js executable path";
+  echo "-s  summary file";
+}
+
+options=":c:l:m:n:s:";
+while getopts $options option; do
+  case $option in
+      E  ) DEPLOYMENT=$OPTARG;;
+      l  ) LOGDIR=$OPTARG;;
+      n  ) NODE=$OPTARG;;
+      s  ) SUMMARYFILENAME=$OPTARG;;
+      h  ) usage; exit;;
+      \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+      :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+      *  ) echo "Unimplimented option: -$OPTARG" >&2; exit 1;;
+  esac
+done
+
+REVNO=`git log -n 1 --pretty=%h`   ## = "bzr revno"
 [ -d $LOGDIR ] || mkdir $LOGDIR
+SUMMARYFILE="$LOGDIR/$SUMMARYFILENAME"
 
 DoRun() {
   ADAPTER=$1
   TIME=`date +%d%b%Y-%H%M%S`
   LOGFILE="$LOGDIR/git-$REVNO-$ADAPTER-$TIME.txt"
   Echo "Running $ADAPTER"
-  node --expose-gc jscrund --adapter=$ADAPTER --modes=indy,bulk -r 8  | tee $LOGFILE
+  OPTS1="--expose-gc jscrund --adapter=$ADAPTER --modes=indy,bulk -r 8"
+  OPTS2="-E $DEPLOYMENT"
+  ${NODE} $OPTS1 $OPTS2 | tee $LOGFILE
 } 
 
 Sum() {
