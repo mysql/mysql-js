3ef5057ef2cd4868a234cca659b40e5bfacfd969
Craig L Russell <craig.russell@oracle.com>
2016-07-27 17:46:56 -0700

    Add JSON tests to jones
    Create tables containing JSON column types
    Test storage in JSON column types
      use for storing javascript objects of all types
      use for storing SPARSE fields

---
+++ database-jones/test/json/DefaultFieldSemistructTest.js
@@ -0,0 +1,133 @@
+/*
+ Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights
+ reserved.
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; version 2 of
+ the License.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ 02110-1301  USA
+ */
+
+"use strict";
+var util    = require("util");
+var udebug  = unified_debug.getLogger("json/DefaultFieldSemistructTest.js");
+
+function Semistruct(id, name, number, a) {
+  if (id !== undefined) {
+    this.id = id;
+    this.name = name;
+    this.number = number;
+    this.a = a;
+  }
+}
+
+var t1 = new harness.SerialTest("ReadDefaultFieldSemistructTest");
+t1.run = function() {
+  var testCase = this;
+  var semistructMapping = new mynode.TableMapping('json_semistruct');
+  semistructMapping.mapSparseFields('SPARSE_FIELDS');
+  semistructMapping.applyToClass(Semistruct);
+
+  testCase.mappings = Semistruct;
+  fail_openSession(testCase, function(session) {
+    testCase.session = session;
+  })
+  .then(function() {
+    return testCase.session.find(Semistruct, 1);
+  })
+  .then(function(found) {
+    // verify found
+    udebug.log("ReadSemistructTest found: " + util.inspect(found));
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify SPARSE_FIELDS", undefined, found.SPARSE_FIELDS);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify id", 1, found.id);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify name", 'Name 1', found.name);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify number", 1, found.number);
+    if (Array.isArray(found.a)) {
+      if (found.a.length === 2) {
+        testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify a[0].a10", 'a10', found.a[0].a10);
+        testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify a[1].a11", 'a11', found.a[1].a11);
+      } else {
+        testCase.error('\n' + testCase.name + " array a has wrong length; expected: 2, actual: " + found.a.length);
+      }
+    } else {
+      testCase.error('\n' + testCase.name + " failed to load property a.");
+    }
+  })
+  // clean up and report errors
+  .then(function() {
+    return testCase.session.close();
+  })
+  .then(function() {testCase.failOnError();}, function(err) {testCase.fail(err);}
+  );
+};
+
+
+var t2 = new harness.SerialTest("WriteDefaultFieldSemistructTest");
+t2.run = function() {
+  var testCase = this;
+  var semistructMapping = new mynode.TableMapping('json_semistruct');
+  semistructMapping.mapSparseFields('SPARSE_FIELDS');
+  semistructMapping.applyToClass(Semistruct);
+
+  function Sparse(id) {}
+
+  var sparseMapping = new mynode.TableMapping('json_semistruct');
+  sparseMapping.mapField({fieldName: 'sparse', columnName: 'SPARSE_FIELDS', converter: mynode.converters.JSONConverter});
+  sparseMapping.applyToClass(Sparse);
+  
+  testCase.mappings = Semistruct;
+  
+  var semistruct10 = new Semistruct(10, 'Name 10', 10, [{a100: 'a100'}, {a101: 'a101'}]);
+  fail_openSession(testCase, function(session) {
+    testCase.session = session;
+  })
+  .then(function() {
+    testCase.session.persist(semistruct10);
+  })
+  .then(function() {
+    return testCase.session.find(Sparse, 10);
+  })
+  .then(function(found) {
+    // verify found
+    var a = found.sparse.a;
+    udebug.log("WriteSemistructTest found: " + util.inspect(found) + '\n' + util.inspect(a));
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify SPARSE_FIELDS", undefined, found.SPARSE_FIELDS);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify id", 10, found.id);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify name", 'Name 10', found.name);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify number", 10, found.number);
+    if (Array.isArray(a)) {
+      if (a.length === 2) {
+        testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify a[0].a100", 'a100', a[0].a100);
+        testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify a[1].a101", 'a101', a[1].a101);
+      } else {
+        testCase.error('\n' + testCase.name + " array a has wrong length; expected: 2, actual: " + a.length);
+      }
+    } else {
+      testCase.error('\n' + testCase.name + " failed to load property a.");
+    }
+    // verify that sparse has no other fields; sparse fields should not include default mapped fields
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify sparse.id", undefined, found.sparse.id);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify sparse.name", undefined, found.sparse.name);
+    testCase.errorIfNotEqual('\n' + testCase.name + " failed to verify sparse.number", undefined, found.sparse.number);
+  })
+  // clean up and report errors
+  .then(function() {
+    return testCase.session.close();
+  })
+  .then(function() {testCase.failOnError();}, function(err) {testCase.fail(err);}
+  );
+};
+
+
+
+exports.tests = [t1, t2];
---
+++ database-jones/test/json/WriteReadTest.js
@@ -0,0 +1,145 @@
+/*
+ Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights
+ reserved.
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; version 2 of
+ the License.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ 02110-1301  USA
+ */
+
+"use strict";
+var util    = require("util");
+var udebug  = unified_debug.getLogger("json/WriteReadTest.js");
+
+function Hybrid(id, unstruct_json, unstruct_varchar, a) {
+  if (id !== undefined) {
+    this.id = id;
+    this.unstruct_json = unstruct_json;
+    this.unstruct_varchar = unstruct_varchar;
+    this.a = a;
+  }
+}
+
+var hybrids = 
+	[
+	 new Hybrid(100), // undefined
+	 new Hybrid(101, null, null), // null
+	 new Hybrid(102, true, true), // boolean
+	 new Hybrid(103, 103.333, 103.333), // number
+	 new Hybrid(104, '104', '104'), // string
+	 new Hybrid(105, ['105', 105], ['105', 105]), // array
+	 new Hybrid(106, {'106': 106}, {'106': 106}), // object
+  ];
+
+/** Try to write JavaScript objects and read back using both a varchar column and a json column.
+ * The JSONConverter is used for both columns.
+ * More data types can be added by adding them to the hybrids array.
+ * Data types: 
+ *   undefined (becomes null by default in database)
+ *   null
+ *   boolean
+ *   numeric
+ *   string
+ *   array object
+ *   non-array object
+ */
+var t1 = new harness.SerialTest("WriteReadTest");
+t1.run = function() {
+  var testCase = this;
+  var hybridMapping = new mynode.TableMapping('json_hybrid');
+  hybridMapping.mapField('id');
+  hybridMapping.mapField('unstruct_json', mynode.converters.JSONConverter);
+  hybridMapping.mapField('unstruct_varchar', mynode.converters.JSONConverter);
+  hybridMapping.mapSparseFields('SPARSE_FIELDS');
+  hybridMapping.applyToClass(Hybrid);
+
+  testCase.mappings = Hybrid;
+  
+  fail_openSession(testCase, function(session) {
+  	udebug.log_detail('WriteReadTest.openSession');
+    testCase.session = session;
+  })
+  .then(function() {
+  	udebug.log_detail('WriteReadTest.persist');
+  	var batch = testCase.session.createBatch();
+    batch.persist(hybrids[0]);
+    batch.persist(hybrids[1]);
+    batch.persist(hybrids[2]);
+    batch.persist(hybrids[3]);
+    batch.persist(hybrids[4]);
+    batch.persist(hybrids[5]);
+    batch.persist(hybrids[6]);
+    return batch.execute();
+  })
+  .then(function() {
+  	udebug.log_detail('WriteReadTest.createQuery')
+  	return testCase.session.createQuery(Hybrid);
+  })
+  .then(function(q) {
+  	q.where(q.id.ge(100));
+  	return q.execute({"order": "asc"});
+  })
+  .then(function(found) {
+  	var i, j, prop, expected, actual;
+    // verify found
+    udebug.log_detail("WriteReadTest found: " + util.inspect(found));
+    for (i = 0; i < hybrids.length; ++i) {
+    	expected = hybrids[i];
+    	actual = found[i];
+      testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify id", expected.id, actual.id);
+    	// most types can be compared directly via === except for array and object
+      if (expected.unstruct_json === undefined || expected.unstruct_json === null) {
+      	// undefined values come back as null for varchar and JSON types
+        testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_json",
+            null, actual.unstruct_json);
+        testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_varchar",
+            null, actual.unstruct_varchar);      	
+      } else if (Array.isArray(expected.unstruct_json)) {
+      	// compare arrays
+      	for (j = 0; j < expected.unstruct_json.length; ++j) {
+          testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_json",
+          		expected.unstruct_json[j], actual.unstruct_json[j]);
+          testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_varchar",
+          		expected.unstruct_varchar[j], actual.unstruct_varchar[j]);      	
+      	}
+      } else if (expected.unstruct_json.constructor.name === 'Object') {
+      	// compare objects
+      	for (prop in expected.unstruct_json) {
+      		if (expected.unstruct_json.hasOwnProperty(prop)) {
+            testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_json",
+            		expected.unstruct_json[prop], actual.unstruct_json[prop]);
+            testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_varchar",
+            		expected.unstruct_varchar[prop], actual.unstruct_varchar[prop]);      	
+      		}
+      	}
+      } else {
+      	// compare directly
+        testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_json",
+            expected.unstruct_json, actual.unstruct_json);
+        testCase.errorIfNotEqual('\n' + testCase.name + ' index ' + i + " failed to verify unstruct_varchar",
+            expected.unstruct_varchar, actual.unstruct_varchar);
+      }
+    }
+  })
+  // clean up and report errors
+  .then(function() {
+    return testCase.session.close();
+  })
+  .then(function() {testCase.failOnError();}, function(err) {console.log(err);testCase.fail(err);}
+  );
+};
+
+
+
+exports.tests = [t1];
