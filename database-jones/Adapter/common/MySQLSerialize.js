a95d02ce34d41f3084724b1c1d32e615f40eaa57
John David Duncan <john.duncan@oracle.com>
2016-08-04 22:15:30 -0700

    wl#8132 serialize/parse: fix code to be compatible with MySQL's implementation

--- database-jones/Adapter/common/MySQLSerialize.js
+++ database-jones/Adapter/common/MySQLSerialize.js
@@ -26,7 +26,7 @@
 
 var assert = require("assert"),
     unified_debug = require("unified_debug"),
-    udebug = unified_debug.getLogger("MySQL_serialize.js"),
+    udebug = unified_debug.getLogger("MySQLSerialize.js"),
 
     TYPE_SMALL_OBJ       =  0x00,
     TYPE_LARGE_OBJ       =  0x01,
@@ -54,33 +54,52 @@ var assert = require("assert"),
     binaryUndefined,
 
     inlineBufsize,
-    keyEntrySizeArray,
-    valueEntrySizeArray,
+    keyEntrySizeArray, valueEntrySizeArray, headerSizeArray,
     serialize;
 
-inlineBufsize = {
-  TYPE_LITERAL : 1, TYPE_INT16   : 2, TYPE_UINT16  : 2,
-  TYPE_INT32   : 4, TYPE_UINT32  : 4
+
+/* Begin Polyfill */
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === "number" &&
+    isFinite(value) &&
+    Math.floor(value) === value;
 };
 
-keyEntrySizeArray = [ 4,6 ];   // small object, large object
-valueEntrySizeArray = [ 3,5,3,5 ];  // small obj, large obj, sm array, lg array
+if(!String.prototype.repeat) {
+  String.prototype.repeat = function(count) {
+    var str = "";
+    while(count-- > 0) { str += this; }
+    return str;
+  };
+}
+/* End Polyfill */
 
+inlineBufsize = { 4:1, 5:2, 6:2, 7:4, 8:4 };  // decimal type => inline size
+keyEntrySizeArray   = [ 4,6 ];       // small object, large object
+valueEntrySizeArray = [ 3,5,3,5 ];   // sm object, lg object, sm array, lg array
+headerSizeArray     = [ 4,8,4,8 ];   // sm object, lg object, sm array, lg array
+
+/* Binary representation of a JavaScript value
+*/
 function Binary(type, jsValue, buffer) {
   assert.notStrictEqual(type, undefined);
+  assert(type <= TYPE_STRING || type === BINARY_KEY);
   this.type = type;
   this.jsValue = jsValue;
   this.buffer  = buffer || null;
-  this.isLarge = (type == TYPE_LARGE_OBJ || type == TYPE_LARGE_ARRAY);
   this.isUndefined = (type === TYPE_LITERAL && jsValue === undefined);
-  this.keyEntrySize = keyEntrySizeArray[this.type];
-  this.valueEntrySize = valueEntrySizeArray[this.type];
+  if(this.type <= TYPE_LARGE_ARRAY) {
+    this.isLarge = (type == TYPE_LARGE_OBJ || type == TYPE_LARGE_ARRAY);
+    this.keyEntrySize = keyEntrySizeArray[this.type];
+    this.valueEntrySize = valueEntrySizeArray[this.type];
+    this.headerSize = headerSizeArray[this.type];
+  }
 }
 
 /* element-count ::= uint16 | uint32 
    size ::= uint16 | uint32
 */
-Binary.prototype.writeElementCountAndSize = function(count, size) {
+Binary.prototype.writeHeader = function(count, size) {
   var buffer = new Buffer(this.isLarge ? 8 : 4);
   if(this.isLarge) {
     buffer.writeUInt32LE(count, 0);
@@ -90,6 +109,31 @@ Binary.prototype.writeElementCountAndSize = function(count, size) {
     buffer.writeUInt16LE(size, 2);
   }
   return buffer;
+};
+
+Binary.prototype.readHeader = function() {
+  var recSize;
+
+  if((this.elementCount !== undefined)  // Already read
+     || (this.type > TYPE_LARGE_ARRAY))  // Value is a scalar
+  {
+    return;
+  }
+
+  if(this.isLarge) {
+    this.elementCount = this.buffer.readUInt32LE(0);
+    recSize = this.buffer.readUInt32LE(4);
+  } else {
+    this.elementCount = this.buffer.readUInt16LE(0);
+    recSize = this.buffer.readUInt16LE(2);
+  }
+
+  this.buffer = this.buffer.slice(0, recSize); // truncate buffer
+
+  if(this.type <= TYPE_LARGE_OBJ) {                         /* Object */
+    this.valueEntryStartPos = this.headerSize +
+      (this.keyEntrySize * this.elementCount);
+  }
 };
 
 Binary.prototype.isInline = function(isLarge) {
@@ -103,6 +147,7 @@ Binary.prototype.writeInline = function(writeBuffer, offset) {
 
 Binary.prototype.readInline = function(sourceBuffer, offset) {
   this.buffer = sourceBuffer.slice(offset, offset + inlineBufsize[this.type]);
+  udebug.log_detail("readInline", this.buffer);
 };
 
 Binary.prototype.setLarge = function() {
@@ -110,11 +155,13 @@ Binary.prototype.setLarge = function() {
   this.isLarge = true;
   this.keyEntrySize = keyEntrySizeArray[this.type];
   this.valueEntrySize = valueEntrySizeArray[this.type];
+  this.headerSize = headerSizeArray[this.type];
 };
 
 Binary.prototype.write = function() {
-  return Buffer.concat([ new Buffer([this.type]), this.buffer ], this.buffer.length+1);
- };
+  return this.isUndefined ? null :
+    Buffer.concat([new Buffer([this.type]), this.buffer], this.buffer.length+1);
+};
 
 
 function VariableLength(length) {
@@ -206,14 +253,6 @@ function serializeInt64(jsNumber) {
   return binary;
 }
 
-/* Begin Polyfill */
-Number.isInteger = Number.isInteger || function(value) {
-  return typeof value === "number" && 
-    isFinite(value) && 
-    Math.floor(value) === value;
-};
-/* End Polyfill */
-
 function serializeNumber(jsNumber) {
   if(! Number.isInteger(jsNumber)) {
     return serializeDouble(jsNumber);
@@ -245,25 +284,26 @@ function ValueEntry(binary, offset) {
 }
 
 /* value-entry ::= type offset-or-inlined-value */
-ValueEntry.prototype.write = function(buffer, cursor, isLarge) {
+// write = function(buffer, offset, cursor, isLarge) {
+ValueEntry.prototype.write = function(buffer, dataStartPos, cursor, isLarge) {
   buffer[cursor++] = this.binary.type;
   if(this.binary.isInline(isLarge)) {
     this.binary.writeInline(buffer, cursor);
   } else if(isLarge) {
-    buffer.writeUInt32LE(this.offset, cursor);
+    buffer.writeUInt32LE(dataStartPos + this.offset, cursor);
   } else {
-    buffer.writeUInt16LE(this.offset, cursor);
+    buffer.writeUInt16LE(dataStartPos + this.offset, cursor);
   }
 };
 
-ValueEntry.prototype.read = function(entryBuffer, cursor, valueBuffer, isLarge) {
-  this.binary = new Binary(entryBuffer[cursor++]);
+ValueEntry.prototype.read = function(buffer, cursor, isLarge) {
+  this.binary = new Binary(buffer[cursor++]);
   if(this.binary.isInline(isLarge)) {
-    this.binary.readInline(entryBuffer, cursor);
+    this.binary.readInline(buffer, cursor);
   } else {
     this.offset = isLarge ?
-      entryBuffer.readUInt32LE(cursor) : entryBuffer.readUInt16LE(cursor);
-    this.binary.buffer = valueBuffer.slice(this.offset);
+      buffer.readUInt32LE(cursor) : buffer.readUInt16LE(cursor);
+    this.binary.buffer = buffer.slice(this.offset);
   }
 };
 
@@ -271,31 +311,64 @@ ValueEntry.prototype.parse = function() {
   return this.binary.parse();
 };
 
-function List(binary, itemSizeArray, itemConstructor) {
+
+function KeyEntry(binary, offset) {
+  this.binary = binary;
+  this.offset = offset;
+  this.length = 0;
+  this.key = "";
+}
+
+/* key-entry ::= key-offset key-length */
+KeyEntry.prototype.write = function(buffer, dataStartPos, cursor, isLarge) {
+  if(isLarge) {
+    buffer.writeUInt32LE(dataStartPos + this.offset, cursor);
+    cursor += 4;
+  } else {
+    buffer.writeUInt16LE(dataStartPos + this.offset, cursor);
+    cursor += 2;
+  }
+  buffer.writeUInt16LE(this.binary.buffer.length, cursor);
+};
+
+KeyEntry.prototype.read = function(buffer, cursor, isLarge) {
+  if(isLarge) {
+    this.offset = buffer.readUInt32LE(cursor);
+    cursor += 4;
+  } else {
+    this.offset = buffer.readUInt16LE(cursor);
+    cursor += 2;
+  }
+  this.length = buffer.readUInt16LE(cursor);
+  this.key = buffer.toString('utf8', this.offset, this.offset + this.length);
+};
+
+
+function List(binary, entrySizeArray, itemConstructor, dataBuffer) {
   this.parent = binary;
-  this.itemSizeArray = itemSizeArray;
-  this.itemSize = itemSizeArray[this.parent.type];
+  this.entrySizeArray = entrySizeArray;
+  this.entrySize = entrySizeArray[this.parent.type];
   this.ItemConstructor = itemConstructor;
-  this.list = [];
-  this.buffer = new Buffer("");
+  this.entries = [];
+  this.data = dataBuffer || new Buffer("");
 }
 
 List.prototype.push = function(binaryForm) {
-  var item = new this.ItemConstructor(binaryForm, this.buffer.length);
-  this.list.push(item);
-  this.buffer = Buffer.concat( [this.buffer, binaryForm.buffer] );
+  var item = new this.ItemConstructor(binaryForm, this.data.length);
+  this.entries.push(item);
+  this.data = Buffer.concat( [this.data, binaryForm.buffer] );
 };
 
-List.prototype.writeEntries = function() {
+List.prototype.writeEntries = function(dataStartPos) {
   var buffer, elemSize, cursor, isLarge;
 
-  elemSize = this.itemSize;
-  buffer = new Buffer(this.list.length * elemSize);
+  elemSize = this.entrySize;
+  buffer = new Buffer(this.entries.length * elemSize);
   cursor = 0;
   isLarge = this.parent.isLarge;
 
-  this.list.forEach(function(entry) {
-    entry.write(buffer, cursor, isLarge);
+  this.entries.forEach(function(entry) {
+    entry.write(buffer, dataStartPos, cursor, isLarge);
     cursor += elemSize;
   });
 
@@ -305,24 +378,25 @@ List.prototype.writeEntries = function() {
 List.prototype.readEntries = function(offset, count) {
   var index;
   for(index = 0; index < count ; index++) {
-    this.list[index] = new this.ItemConstructor();
-    this.list[index].read(this.parent.buffer,
-                          offset + (index * this.itemSize),
-                          this.buffer, this.parent.isLarge);
+    this.entries[index] = new this.ItemConstructor();
+    this.entries[index].read(this.parent.buffer,
+                          offset + (index * this.entrySize),
+                          this.parent.isLarge);
   }
 };
 
+List.prototype.getSizeOfEntries = function() {
+  return this.entrySize * this.entries.length;
+};
+
 List.prototype.getTotalSize = function() {
-  this.itemSize = this.itemSizeArray[this.parent.type];  // reset
-  if(this.list.length) {
-    return this.buffer.length + (this.itemSize * this.list.length);
-  }
-  return 0;
+  this.entrySize = this.entrySizeArray[this.parent.type];  // recalculate
+  return this.getSizeOfEntries() + this.data.length;
 };
 
 List.prototype.parse = function() {
   var result = [];
-  this.list.forEach(function(item) {
+  this.entries.forEach(function(item) {
     result.push(item.parse());
   });
   return result;
@@ -343,66 +417,33 @@ function serializeArray(jsArray) {
     valueList.push(bin);
   });
 
-  size = valueList.getTotalSize();
+  size = binary.headerSize + valueList.getTotalSize();
   if(size > 65535) {
     binary.setLarge();
-    size = valueList.getTotalSize();  // recalculates with isLarge=true
+    size = binary.headerSize + valueList.getTotalSize();  // recalculates
   }
 
   /* array ::= element-count size value-entries values */
   binary.buffer = Buffer.concat( [
-    binary.writeElementCountAndSize(valueList.list.length, size),
-    valueList.writeEntries(),
-    valueList.buffer
+    binary.writeHeader(valueList.entries.length, size),
+    valueList.writeEntries(binary.headerSize + valueList.getSizeOfEntries()),
+    valueList.data
   ] );
 
   return binary;
 }
 
 
-function KeyEntry(binary, offset) {
-  this.binary = binary;
-  this.offset = offset;
-  this.length = 0;
-  this.key = "";
-}
-
-/* key-entry ::= key-offset key-length */
-KeyEntry.prototype.write = function(buffer, cursor, isLarge) {
-  if(isLarge) {
-    buffer.writeUInt32LE(this.offset, cursor);
-    cursor += 4;
-  } else {
-    buffer.writeUInt16LE(this.offset, cursor);
-    cursor += 2;
-  }
-  buffer.writeUInt16LE(this.binary.buffer.length, cursor);
-};
-
-KeyEntry.prototype.read = function(entryBuffer, cursor, valueBuffer, isLarge) {
-  if(isLarge) {
-    this.offset = entryBuffer.readUInt32LE(cursor);
-    cursor += 4;
-  } else {
-    this.offset = entryBuffer.readUInt16LE(cursor);
-    cursor += 2;
-  }
-  this.length = entryBuffer.readUInt16LE(cursor);
-  if(Buffer.isBuffer(valueBuffer)) {
-    this.key = valueBuffer.toString('utf8', this.offset, this.offset + this.length);
-  }
-};
-
-/* When serializing an object, keys are sorted on length, and keys 
+/* When serializing an object, keys are sorted on length, and keys
    with the same length are sorted lexicographically */
 function sortKeys(a,b) {
   if(a.length > b.length) {
-    return -1;
+    return 1;
   }
   if(a.length === b.length) {
     return (a < b) ? -1 : 1;
   }
-  return 1;
+  return -1;
 }
 
 /* key ::= utf8-data */
@@ -418,6 +459,7 @@ function serializeObject(jsObject) {
   var sortedValues = [];
   var validityCheck = [];
   var size;
+  var valueEntryStartPos, keyDataStartPos, valueDataStartPos;
 
   /* Build an ordered list of keys */
   sortedKeys = Object.keys(jsObject).sort(sortKeys);
@@ -444,21 +486,24 @@ function serializeObject(jsObject) {
     }
   });
 
-  size = valueList.getTotalSize() + keyList.getTotalSize();
+  size = binary.headerSize + valueList.getTotalSize() + keyList.getTotalSize();
   if(size > 65535) {
-    binary.setLarge();
-    size = valueList.getTotalSize() + keyList.getTotalSize();   // recalculate
+    binary.setLarge();  // then recalculate sizes:
+    size = binary.headerSize + valueList.getTotalSize() + keyList.getTotalSize();
   }
 
   /* object ::=  element-count size key-entries value-entries keys values */
+  valueEntryStartPos = binary.headerSize + keyList.getSizeOfEntries();
+  keyDataStartPos = valueEntryStartPos+ valueList.getSizeOfEntries();
+  valueDataStartPos = keyDataStartPos + keyList.data.length;
   binary.buffer = Buffer.concat( [
-    binary.writeElementCountAndSize(valueList.list.length, size),
-    keyList.writeEntries(),
-    valueList.writeEntries(),
-    keyList.buffer,
-    valueList.buffer
+    binary.writeHeader(valueList.entries.length, size),
+    keyList.writeEntries(keyDataStartPos),
+    valueList.writeEntries(valueDataStartPos),
+    keyList.data,
+    valueList.data
   ] );
-
+  assert.equal(binary.buffer.length, size);
   return binary;
 }
 
@@ -587,72 +632,33 @@ Binary.prototype.parseString = function() {
    array ::= element-count size value-entries values
    object ::=  element-count size key-entries value-entries keys values
 */
-Binary.prototype.setupParser = function() {
-  var recSize, keyEntriesLength, valEntriesLength, headerSize;
-  var finalKey;
-
-  if((this.elementCount !== undefined)  // Parser has already been initialized
-     || (this.type > TYPE_LARGE_ARRAY))  // Value is a scalar
-  {
-    return;
-  }
-
-  if(this.isLarge) {
-    this.elementCount = this.buffer.readUInt32LE(0);
-    recSize = this.buffer.readUInt32LE(4);
-    headerSize = 8;
-  } else {
-    this.elementCount = this.buffer.readUInt16LE(0);
-    recSize = this.buffer.readUInt16LE(2);
-    headerSize = 4;
-  }
-
-  valEntriesLength = this.valueEntrySize * this.elementCount;
-  this.buffer = this.buffer.slice(0, headerSize + recSize); // truncate buffer
-  if(this.type <= TYPE_LARGE_OBJ) {                         /* Object */
-    this.keyEntryStartPos = headerSize;
-    keyEntriesLength = this.keyEntrySize * this.elementCount;
-    this.valueEntryStartPos = headerSize + keyEntriesLength;
-    this.keyDataStartPos = this.valueEntryStartPos + valEntriesLength;
-    finalKey = new KeyEntry();
-    finalKey.read(this.buffer,
-                  this.valueEntryStartPos - this.keyEntrySize,
-                  null, this.isLarge);
-    this.valueDataStartPos = this.keyDataStartPos + finalKey.offset + finalKey.length;
-  } else {                                                  /* Array */
-    this.valueEntryStartPos = headerSize;
-    this.valueDataStartPos = headerSize + valEntriesLength;
-  }
-};
 
 Binary.prototype.parseArray = function() {
   var valueList;
 
-  this.setupParser();
+  this.readHeader();
   valueList = new List(this, valueEntrySizeArray, ValueEntry);
-  valueList.buffer = this.buffer.slice(this.valueDataStartPos);
-  valueList.readEntries(this.valueEntryStartPos, this.elementCount);
+  valueList.readEntries(this.headerSize, this.elementCount);
   return valueList.parse();
 };
 
 Binary.prototype.parseObject = function() {
   var keyList, valueList, result, i;
 
-  this.setupParser();
+  this.readHeader();
   valueList = new List(this, valueEntrySizeArray, ValueEntry);
   keyList = new List(this, keyEntrySizeArray, KeyEntry);
 
   if(this.elementCount > 0) {
-    keyList.buffer = this.buffer.slice(this.keyDataStartPos);
-    keyList.readEntries(this.keyEntryStartPos, this.elementCount);
-    valueList.buffer = this.buffer.slice(this.valueDataStartPos);
+    keyList.readEntries(this.headerSize, this.elementCount);
     valueList.readEntries(this.valueEntryStartPos, this.elementCount);
   }
 
   result = {};
   for(i = 0 ; i < this.elementCount ; i++) {
-    result[keyList.list[i].key] = valueList.list[i].parse();
+    result[keyList.entries[i].key] = valueList.entries[i].parse();
   }
+  udebug.log_detail("parseObject result:", result);
   return result;
 };
 
@@ -691,7 +697,10 @@ exports.serialize = function(jsValue) {
 };
 
 function getBinaryForBuffer(sourceBuffer) {
-  return new Binary(sourceBuffer[0], undefined, sourceBuffer.slice(1));
+  if(Buffer.isBuffer(sourceBuffer) && sourceBuffer.length > 1) {
+    return new Binary(sourceBuffer[0], undefined, sourceBuffer.slice(1));
+  }
+  return binaryUndefined;
 }
 
 /* public parse()
@@ -699,7 +708,8 @@ function getBinaryForBuffer(sourceBuffer) {
    returns a JS object
 */
 exports.parse = function(sourceBuffer) {
-  return getBinaryForBuffer(sourceBuffer).parse();
+  return Buffer.isBuffer(sourceBuffer) ?
+    getBinaryForBuffer(sourceBuffer).parse() : null;
 };
 
 exports.getUnitTestValues = function() {
@@ -744,7 +754,8 @@ exports.getUnitTestValues = function() {
     Math,                             // language-defined object
     largeObject,
     largeArray,
-    longString
+    longString,
+    {"a":[{"a10":"a10"},{"a11":"a11"}],"name":"Name 1","number":1}
   ];
 };
 
@@ -766,4 +777,3 @@ function runUnitTests() {
 }
 
 // runUnitTests();
-
