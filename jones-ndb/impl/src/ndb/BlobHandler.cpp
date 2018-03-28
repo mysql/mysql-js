ee076d8b2eb6e3a78c66374a92f7d183672be279
John David Duncan <john.duncan@oracle.com>
2018-03-28 13:47:51 -0700

    Update files with longer GPL text and appropriate copyright notices Changes copied from the mysql source tree

--- jones-ndb/impl/src/ndb/BlobHandler.cpp
+++ jones-ndb/impl/src/ndb/BlobHandler.cpp
@@ -1,21 +1,25 @@
 /*
- Copyright (c) 2016, Oracle and/or its affiliates. All rights
- reserved.
+ Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
  
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; version 2 of
- the License.
- 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License, version 2.0,
+ as published by the Free Software Foundation.
+
+ This program is also distributed with certain software (including
+ but not limited to OpenSSL) that is licensed under separate terms,
+ as designated in a particular file or component or in included license
+ documentation.  The authors of MySQL hereby grant you an additional
+ permission to link the program and your derivative works with the
+ separately licensed software that they have included with MySQL.
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License, version 2.0, for more details.
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- 02110-1301  USA
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
 #include <stdlib.h>
@@ -73,7 +77,7 @@ int BlobReadHandler::runActiveHook(NdbBlob *b) {
     content = (char *) malloc(length);                        // here is malloc
     if(content) {
       int rv = ndbBlob->readData(content, nBytes);
-      DEBUG_PRINT("BLOB read: column %d, length %d, read %d/%d", 
+      DEBUG_PRINT("BLOB read: column %d, length %llu, read %d/%d", 
                   columnId, length, rv, nBytes);
     } else {
       return -1;
@@ -112,7 +116,7 @@ void BlobWriteHandler::prepare(const NdbOperation * ndbop) {
     assert(false);
   }
 
-  DEBUG_PRINT("Prepare write for BLOB column %d, length %d", columnId, length);
+  DEBUG_PRINT("Prepare write for BLOB column %d, length %llu", columnId, length);
   ndbBlob->setValue(content, static_cast<uint32_t>(length));
   if(next) next->prepare(ndbop);
 }
