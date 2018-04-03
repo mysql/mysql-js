bd89461381f420726bd7d1b572423c1f5ce8f41f
John David Duncan <john.duncan@oracle.com>
2018-04-03 16:40:48 -0700

    Compile fix for jones-ndb builds outside of mysql tree with cluster 7.6
    Add include of my_global.h

--- jones-ndb/impl/src/ndb/EncoderCharset.cpp
+++ jones-ndb/impl/src/ndb/EncoderCharset.cpp
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
+ Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
  
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License, version 2.0,
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <my_global.h>
 #include <mysql.h>
 #include <my_sys.h>
 
