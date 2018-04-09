cbe61e9e7abd82d59fdd87745fdbea967d8e0bec
John David Duncan <john.duncan@oracle.com>
2018-04-09 10:50:24 -0700

    jones-ndb: Adapt uses of V8 API for better compatibility with new versions
    Use SetDataProperty() on template rather than SetAccessor()

--- jones-ndb/impl/include/common/JsWrapper.h
+++ jones-ndb/impl/include/common/JsWrapper.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
+ Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
  
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License, version 2.0,
@@ -214,7 +214,7 @@ public:
   }
 
   void addAccessor(const char *name, Getter accessor) {
-    stencil.Get(isolate)->SetAccessor(
+    stencil.Get(isolate)->SetNativeDataProperty(
       String::NewFromUtf8(isolate, name, v8::String::kInternalizedString),
       accessor
     );
